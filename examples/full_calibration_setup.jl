8e7c096fd7eae0514c0b2527c030fc2e2417d345
adelinehillier <adelineh2000@gmail.com>
2022-11-30 13:37:30 -0500

    Switch to new LESbrary data

--- examples/full_calibration_setup.jl
+++ examples/full_calibration_setup.jl
@@ -47,44 +47,51 @@ if datadep
         )
 
 else
-    data_dir = "../../../../home/greg/Projects/LocalOceanClosureCalibration/data"
-    one_day_suite_path_1m(case) = data_dir * "/one_day_suite/1m/$(case)_instantaneous_statistics.jld2"
-    two_day_suite_path_1m(case) = data_dir * "/two_day_suite/1m/$(case)_instantaneous_statistics.jld2"
-    one_day_suite_path_2m(case) = data_dir * "/one_day_suite/2m/$(case)_instantaneous_statistics.jld2"
-    one_day_suite_path_4m(case) = data_dir * "/one_day_suite/4m/$(case)_instantaneous_statistics.jld2"
+    data_dir = "../../../../home/greg/Projects/SingleColumnModelCalibration.jl/data"
+    half_day_suite_path_1m(case) = data_dir * "/12_hour_suite/1m/$(case)_instantaneous_statistics.jld2"
+    half_day_suite_path_2m(case) = data_dir * "/12_hour_suite/2m/$(case)_instantaneous_statistics.jld2"
+    half_day_suite_path_4m(case) = data_dir * "/12_hour_suite/4m/$(case)_instantaneous_statistics.jld2"
+    one_day_suite_path_1m(case) = data_dir * "/24_hour_suite/1m/$(case)_instantaneous_statistics.jld2"
+    one_day_suite_path_2m(case) = data_dir * "/24_hour_suite/2m/$(case)_instantaneous_statistics.jld2"
+    one_day_suite_path_4m(case) = data_dir * "/24_hour_suite/4m/$(case)_instantaneous_statistics.jld2"
+    two_day_suite_path_1m(case) = data_dir * "/48_hour_suite/1m/$(case)_instantaneous_statistics.jld2"
+    two_day_suite_path_2m(case) = data_dir * "/48_hour_suite/2m/$(case)_instantaneous_statistics.jld2"
+    two_day_suite_path_4m(case) = data_dir * "/48_hour_suite/4m/$(case)_instantaneous_statistics.jld2"
 
     training_path_fns_for_noise_cov_estimate = [one_day_suite_path_1m, one_day_suite_path_2m, one_day_suite_path_4m]
 
     regrid_coarse = 32 # 8 m
     regrid_med = 48 # 5.3 m
     regrid_fine = 64 # 4 m
-    weights = (1.5, 1, 0.75)
+    # weights = (1.5, 1, 0.75)
 
     # regrid_coarse = 16 
     # regrid_med = 32
     # regrid_fine = 48 
-    # weights = (2.0, 1, 0.667)
+    # weights = (2, 1, 0.667)
 
-    # weights = (2, 1, 0.5)
-    # weights = (1,)
-    Δt = 5minutes
+    weights = (regrid_med / regrid_coarse, 1, regrid_med / regrid_fine)
+
+    Δt = 10minutes
 
     # regrid = RectilinearGrid(size=48; z=(-256, 0), topology=(Flat, Flat, Bounded))
     regrid = [RectilinearGrid(size=size; z=(-256, 0), topology=(Flat, Flat, Bounded)) for size in (regrid_coarse, regrid_med, regrid_fine)]
 
     description = "Calibrating to 1-day suite."
 
-    training_times = [0.125days, 0.5days, 0.75days, 1.0days]
-    testing_times = [0.25days, 0.5days, 1.0days, 2.0days]
+    # training_times = [0.25days, 0.5days, 0.75days, 1.0days]
+    # testing_times = [0.25days, 0.5days, 1.0days, 2.0days]
+    training_times = [0.5days, 1.0days]
+    testing_times = [0.5days, 1.0days, 2.0days]
 
     training_path_fn = one_day_suite_path_2m
-    # testing_path_fn = two_day_suite_path_2m
+    testing_path_fn = two_day_suite_path_2m
 
     transformation = (b = Transformation(normalization=ZScore()),
-                    u = Transformation(normalization=ZScore()),
-                    v = Transformation(normalization=ZScore()),
-                    e = Transformation(normalization=RescaledZScore(0.1)),
-                    )
+                      u = Transformation(normalization=ZScore()),
+                      v = Transformation(normalization=ZScore()),
+                      e = Transformation(normalization=RescaledZScore(0.1)),
+                      )
 
     fields_by_case = Dict(
                     "med_wind_med_cooling" => (:b, :u, :v, :e),
